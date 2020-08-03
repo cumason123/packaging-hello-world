ac041df7857eb737ac8de3b85ac2e51feba38673
Curtis Mason <cumason@google.com>
2020-08-03 13:37:44 -0700

    pypi package

--- pypi_packaging.py
+++ pypi_packaging.py
@@ -6,13 +6,18 @@ import os
 _PACKAGE_PYPI_VERSION="1.0.0"
 
 def createTag():
+    # version function only works on python3.8
+    # this pypi_version should be the most updated version. github workflow
+    # isntalls most updated package
     pypi_version = version('cumason-helloworld-package')
+
     if _PACKAGE_PYPI_VERSION == pypi_version:
+        # Create tag and push tag to master
         repo = Repo(os.getcwd())
         repo.create_tag(_PACKAGE_PYPI_VERSION)
         repo.remote().push(_PACKAGE_PYPI_VERSION)
     else:
-        # PyPI workflow publish failed
+        # PyPI publish likely failed
         exit(1)
 
 
