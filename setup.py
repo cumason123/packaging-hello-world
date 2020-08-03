3930211493d460bea67cc56682661589bc04313f
Curtis Mason <cumason@google.com>
2020-08-03 14:41:54 -0700

    setup.py change

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup
 from pypi_packaging import _LOCAL_PYPI_VERSION
 
 
-setup(name="cumason-helloworld-package", version=_LOCAL_PYPI_VERSION)
+setup(version=_LOCAL_PYPI_VERSION)
