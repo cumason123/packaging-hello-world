733df1cf36266026fcfe424309f13f6125252398
Curtis Mason <cumason@google.com>
2020-08-03 12:52:08 -0700

    v3.1.0

--- pypi_packaging.py
+++ pypi_packaging.py
@@ -1,13 +1,19 @@
+from importlib.metadata import version
 from git import Repo
 import os
 
 
-_CLOUDEVENTS_PYPI_VERSION="v3.0.0"
+_CLOUDEVENTS_PYPI_VERSION="v3.1.0"
 
 def createTag():
-    repo = Repo(os.getcwd())
-    repo.create_tag(_CLOUDEVENTS_PYPI_VERSION)
-    repo.remote().push(_CLOUDEVENTS_PYPI_VERSION)
+    pypi_version = version('cumason-helloworld-package')
+    if _CLOUDEVENTS_PYPI_VERSION.endswith(pypi_version):
+        repo = Repo(os.getcwd())
+        repo.create_tag(_CLOUDEVENTS_PYPI_VERSION)
+        repo.remote().push(_CLOUDEVENTS_PYPI_VERSION)
+    else:
+        # PyPI workflow publish failed
+        exit(1)
 
 
 if __name__ == '__main__':
