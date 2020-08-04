4215c6ce15201f7628ae066474ac2a171adc9627
Curtis Mason <cumason@google.com>
2020-08-04 11:42:15 -0400

    removed setup.cfg

--- setup.py
+++ setup.py
@@ -2,4 +2,17 @@ from setuptools import setup
 from pypi_packaging import _LOCAL_PYPI_VERSION
 
 
-setup(version=_LOCAL_PYPI_VERSION)
+setup(
+    name="cumason123-helloworld",
+    summary="Example automated pypi packaging",
+    long_description_content_type="text/markdown",
+    author="Curtis Mason",
+    author_email="cumason@bu.edu",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages="src/helloworld",
+    version=_LOCAL_PYPI_VERSION
+)
